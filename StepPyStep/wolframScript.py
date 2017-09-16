d419d148f3f410d687e62d3a0c03404cdaff62c4
Vineet <vince.parikh@gmail.com>
2017-09-16 12:46:48 -0400

    Encapsulated previous files for older project in folder

--- StepPyStep/wolframScript.py
+++ StepPyStep/wolframScript.py
@@ -2,4 +2,5 @@ import wolframalpha
 client = wolframalpha.Client("L7QYU9-K6EXU2794L")
 mekaInput = "integrate(x^2,x)" #this will later be what the network returns
 res = client.query(mekaInput) #this is the query to wolframalpha
+
 print(next(res.results).text) #test: prints plaintext of all results
