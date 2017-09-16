cd73f73748cead2477a175f13b493dac37970d8d
henrytso <31811065+henrytso@users.noreply.github.com>
2017-09-16 15:37:56 -0400

    Adding site.html

---
+++ DatabaseScripts/make_account.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+# debug mode
+import cgib
+cgitb.enable()
+
+
+# connects to Accounts database
+import pymysql
+accGetter = pymysql.connect(
+    db='Accounts',
+    user='root',
+    passwd='root',
+    host='localhost')
+account=acc.cursor()
+account.execute("INSERT INTO Account_Mapping VALUES (Null, 0)")
+accGetter.commit()
+
+account.execute("SELECT MAX(user_id) FROM Account_Mapping")
+print("Your user id is:")
+print([(r[0]) for r in account.fetchall()])
+print("Make sure to copy and paste this user id when uploading stuff!")
