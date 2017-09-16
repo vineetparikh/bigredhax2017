fed115e5a2edb2ce7b2001cf1d223ee68e244495
Vineet <vince.parikh@gmail.com>
2017-09-16 18:19:52 -0400

    Edited to include tags and classes

--- DatabaseScripts/upload_notes.py
+++ DatabaseScripts/upload_notes.py
@@ -15,8 +15,9 @@ link="bigrednotes.azurewebsites.net/library/"+user_id+"_"+name
 name="image"
 vote_num=0
 download_num=0
-
+tag="List"
+school_class="Memology"
 # Inputs values into repo
-account.execute("INSERT INTO Images VALUES (user_id,link,name,vote_num,download_num)")
+account.execute("INSERT INTO Images VALUES (user_id,link,name,vote_num,download_num,tag,school_class)")
 accGetter.commit()
 print("Successfully uploaded!")
