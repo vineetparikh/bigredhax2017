a4e93bb360a116383bcab81f7aef840a08af7107
Vineet <vince.parikh@gmail.com>
2017-09-18 14:59:50 -0400

    Why did this not get committed yet?

--- clutch.py
+++ clutch.py
@@ -109,9 +109,14 @@ def jsonToTxt(jsonString):
     return stronk
 
 stank = jsonToTxt(jsonInput)
+############
 
-###########
-# This will find the keywords in stank
-import rake
-import operator
-rake_object=rake.Rake("")
+from rake_nltk import Rake
+
+r = Rake() # Uses stopwords for english from NLTK, and all puntuation characters.
+
+# If you want to provide your own set of stop words and punctuations to
+# r = Rake(<list of stopwords>, <string of puntuations to ignore>)
+r.extract_keywords_from_text(stank)
+
+r.get_ranked_phrases() # To get keyword phrases ranked highest to lowest.
